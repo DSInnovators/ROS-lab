baa5f39bc3ec03c3e2ef0e56e99c1efc9fc32cce
mazharcse <mazhar_cse@yahoo.com>
2022-02-04 11:06:06 +0600

    script added to run the project

---
+++ ROS1/catkin_ws/src/gazebo_vehicle_contorl/run_gazebo_vehicle_control.sh
@@ -0,0 +1,15 @@
+echo "running model_push"
+
+roscore &
+sleep 10
+
+rosrun gazebo_ros gzserver  /home/mazharul/code/ROS-lab/ROS1/catkin_ws/src/gazebo_vehicle_contorl/worlds/vehicle_control.world --verbose &
+sleep 10
+
+gzclient &
+
+sleep 10
+rviz rviz -f camera_link &
+
+sleep 10
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py cmd_vel:=vehicle/cmd_vel
