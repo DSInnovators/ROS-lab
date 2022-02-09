980949748b01ec3309a65aae0e81874a293ab7a0
git config --global user.name <tanjan.sj@gmail.com>
2022-02-09 18:43:58 +0600

    feature: run and venv creation script

---
+++ ROS2/run_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Running gazebo"
+echo "######################"
+
+if pgrep -x "gzserver" > /dev/null
+then
+    echo "Running"
+else
+    echo "killall gzserver"
+    gnome-terminal -e "gazebo --verbose world/gazebo_ros_diff_drive_demo.world" 
+fi
+
+gnome-terminal -e "python3 publishers/teleop_keyboard.py"
