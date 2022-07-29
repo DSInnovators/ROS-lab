2714786426d1cd12ce137a0ce8199caa824ad9cf
Enan Ajmain <3nan.ajmain@gmail.com>
2022-07-30 02:14:38 +0600

    Fix: handle lane splitting
    Problem: Lane-follower can't handle when the current lane splits into
    two lanes. Because it takes the component centroid closest to the
    previous iteration's, when the lane splits up, it can't accurately
    decide which white line is the edge of the lane it's currently
    following.
    
    Solution: Using bit masking, erase the right line, so the vehicle only
    sees the left lane and the dashed line, effectively making the left lane
    the only lane.
    
    Pitfall: The region used for bit-masking is hard-coded for this specific
    road, may not work as a general solution.

--- ROS1/catkin_ws/src/lane_follower_sim/auto_drive/src/automate_vehicle.cc
+++ ROS1/catkin_ws/src/lane_follower_sim/auto_drive/src/automate_vehicle.cc
@@ -23,27 +23,42 @@ int error;
 double PI = atan(1)*4;
 bool SHOW_FRAME = true;
 
+int mask(const cv::Mat& img, cv::Mat& ret)
+{
+  if (img.empty()) {
+    return 1;
+  }
+  cv::Mat mask = cv::Mat::zeros(img.size(), img.type());
+
+  cv::Point pts[4] = {
+    cv::Point(0, img.rows),
+    cv::Point(200, img.rows/2),
+    cv::Point(450, img.rows/2),
+    cv::Point(img.cols, img.rows)
+  };
+  cv::fillConvexPoly(mask, pts, 4, cv::Scalar(255, 255, 255));
+
+  cv::bitwise_and(img, mask, ret);
+  return 0;
+}
+
 void imageCallback(const sensor_msgs::ImageConstPtr &msg)
 {
-  cv::Mat frame, gray, dst;
+  cv::Mat frame, gray, clipped, masked;
 
   frame = cv_bridge::toCvShare(msg, "bgr8")->image;
   gray = cv_bridge::toCvShare(msg, "mono8")->image;
 
-  // gray = gray + 100 - mean(gray)[0];
   cv::threshold(gray, gray, 160, 255, cv::THRESH_BINARY);
 
-  dst = gray(cv::Rect(0, gray.rows / 3 * 2, gray.cols, gray.rows / 3));
+  if (mask(gray, masked)) {
+    return;
+  }
 
-  // cv::imwrite("dst.png", dst);
+  clipped = masked(cv::Rect(0, masked.rows / 3 * 2, masked.cols, masked.rows / 3));
 
   cv::Mat labels, stats, centroids;
-  int cnt = cv::connectedComponentsWithStats(dst, labels, stats, centroids);
-
-  // std::cout << cnt << " ";
-  // std::cout << "\n" << labels << "\n";
-  // std::cout << "\n" << stats << "\n";
-  // std::cout << "\n" << centroids << "\n\n\n";
+  int cnt = cv::connectedComponentsWithStats(clipped, labels, stats, centroids);
 
   // the first componenet is the background layer
   // which will always be present
@@ -73,23 +88,23 @@ void imageCallback(const sensor_msgs::ImageConstPtr &msg)
 
   frame_pt.x = (centroid[0].x + centroid[1].x) / 2;
   frame_pt.y = (centroid[0].y + centroid[1].y) / 2 + gray.rows / 3 * 2;
-  cvtColor(dst, dst, cv::COLOR_GRAY2BGR);
+  cvtColor(clipped, clipped, cv::COLOR_GRAY2BGR);
 
   cv::circle(frame, frame_pt, 2, cv::Scalar(0, 0, 255), 2);
-  cv::circle(dst, centroid[0], 2, cv::Scalar(0, 255, 0), 2);
-  cv::circle(dst, centroid[1], 2, cv::Scalar(255, 0, 0), 2);
+  cv::circle(clipped, centroid[0], 2, cv::Scalar(0, 255, 0), 2);
+  cv::circle(clipped, centroid[1], 2, cv::Scalar(255, 0, 0), 2);
 
-  error = dst.cols / 2 - frame_pt.x;
+  error = clipped.cols / 2 - frame_pt.x;
 
   if (SHOW_FRAME) {
     imshow("frame", frame);
-    imshow("dst", dst);
+    imshow("clipped", clipped);
     cv::waitKey(1);
   }
 
   geometry_msgs::Twist cmd_vel;
-  cmd_vel.linear.x = 0.5;
-  cmd_vel.angular.z = (error*PI/frame.cols);
+  cmd_vel.angular.z = (error*PI/gray.cols);
+  cmd_vel.linear.x = 0.3;
 
   pub.publish(cmd_vel);
 }
